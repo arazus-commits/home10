4e93ef337d9fa5d13a9ba4820c7bd4bd1f5b35aa
arazus-commits <araz.osmanov@gmail.com>
2026-04-06 20:31:53 +0300

    пуш

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,19 @@
 
 public class Main {
     public static void main(String[] args) {
+        //задача 1
+        String firstName = " Ivan";
+        String middleName = " Ivanov";
+        String lastName = " Ivanovich";
+        String fullName = middleName + firstName + lastName;
+        System.out.println(fullName);
+
+        //задача 2
+        System.out.println("Данные Ф. И. О. сотрудника для заполнения отчета — " + fullName.toUpperCase());
+
+        //задача 3
+        String fullName1 = "Иванов Семён Семёнович";
+        System.out.println("Данные Ф. И. О. сотрудника — " + fullName1.replace('ё', 'e'));
 
     }
 }
